556d9a5f9fbfee671d3075b2329f8b3f210398a2
Dimon <lovedaih@gamil.com>
2015-11-02 20:33:12 +0800

    AndroidAnnotations框架Demo
    AndroidAnnotations框架在Android Studio中的build.gradle配置Demo。

---
+++ app/src/main/java/com/dimon/org/androidannotationsdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dimon.org.androidannotationsdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.main)
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
